5e862132175a05b8128856d6dcc2692e5c48c504
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2024-11-29 03:00:34 +0530

    script_test

--- test_rl.py
+++ test_rl.py
@@ -7,6 +7,19 @@ from stable_baselines3 import DDPG, DQN
 from stable_baselines3.common.callbacks import BaseCallback, CheckpointCallback
 
 from env import Environment
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Parse action arguments")
+    parser.add_argument(
+        "--action", nargs=2, type=int, required=True, help="Action list of 2 integers"
+    )
+    args = parser.parse_args()
+    return args.action
+
+
+action = parse_args()
 
 train_file_list = [f"dataset/train-{i}" for i in range(1, 51)]
 
@@ -22,7 +35,6 @@ env = Environment(train_file_list, chunk_sizes)
 # Load or initialize the model
 model = DDPG.load("ddpg_transformer_100")  # Load the pre-trained model
 
-action = [4, 4]
 chunk_file = "dataset/train-51"
 observation, _ = env.reset()
 observation, reward, done, _, info = env.step(action, chunk_file)
