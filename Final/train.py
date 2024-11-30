608304713cbb54fb030deff3a1af339a215aa15b
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2024-11-30 06:24:43 +0530

    dtype

--- Final/train.py
+++ Final/train.py
@@ -87,7 +87,6 @@ def train_model(action, train_folder):
     loss = 0
     for item in dataModule.train:
         item = item.to(device)
-        item = item.to(config.dtype)
         item = item.unsqueeze(0)
         print(f"item shape: {item.shape}")
         loss += model.training_step(item, 0).item()
