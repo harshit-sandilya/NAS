251e10cf2d3c1176b27e9353af6064e1355c86fd
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2024-11-30 09:19:49 +0530

    Refactoring

--- run-script.sh
+++
@@ -1,7 +0,0 @@
-pip install -r requirements.txt
-
-for i in {51..100}
-do
-    echo "Running iteration $i..."
-    { time python train-rl.py > output$i.log 2>&1; } 2>> time.log
-done
--- test.sh
+++
@@ -1,5 +0,0 @@
-python test_rl.py --action 4 4 > output_51_44.log 2>&1
-python test_rl.py --action 3 4 > output_51_34.log 2>&1
-python test_rl.py --action 4 3 > output_51_43.log 2>&1
-python test_rl.py --action 5 4 > output_51_54.log 2>&1
-python test_rl.py --action 4 5 > output_51_45.log 2>&1
