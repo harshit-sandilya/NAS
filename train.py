49b979831210f43a131112518aacc9e68f9a622f
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2024-12-02 01:21:33 +0530

    fix

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def train_model(action, config, dataModule):
 
     print("+++++++++++++++++CONFIG+++++++++++++++++")
     print(config.train)
-    print(config.deepseed)
+    print(config.deepspeed)
     print("++++++++++++++++++++++++++++++++++++++++")
 
     start_time = measure_time()
