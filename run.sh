a66637aeaa3224273c2ca8e41609e59d16b1f7ed
Harshit-Sandilya <2021ucp1382@mnit.ac.in>
2024-12-18 15:49:15 +0530

    fixes

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 for i in {1..10}; do
-    python train-rl.py --mode $((i % 2)+1) > output$i.log 2>&1
+    if ((i % 2)); then
+        python train-rl.py --mode 1 > output$i.log 2>&1
+    else
+        python train-rl.py --mode 2 > output$i.log 2>&1
+    fi
 done
